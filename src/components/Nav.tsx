babbe89c60cb9f3e592beb4d8834db5c40584f68
xtp111 <129745214+xtp111@users.noreply.github.com>
2026-03-09 06:19:21 +0800

    first submit

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,40 +2,16 @@ import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
-    <nav>
-      <ul className="nav-list">
-        <li>
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/education" className="nav-link">
-            Education
-          </Link>
-        </li>
-        <li>
-          <Link to="/experience" className="nav-link">
-            Experience
-          </Link>
-        </li>
-        <li>
-          <Link to="/certifications" className="nav-link">
-            Certifications
-          </Link>
-        </li>
-        <li>
-          <Link to="/projects" className="nav-link">
-            Projects
-          </Link>
-        </li>
-        <li>
-          <Link to="/references" className="nav-link">
-            References
-          </Link>
-        </li>
-      </ul>
-    </nav>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/education">Education</Link></li>
+          <li><Link to="/experience">Experience</Link></li>
+          <li><Link to="/certifications">Certifications</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+          <li><Link to="/references">References</Link></li>
+        </ul>
+      </nav>
   );
 }
 
