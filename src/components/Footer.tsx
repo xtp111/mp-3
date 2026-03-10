f3febdf864741de46cae60bd8f5fc026858c34a3
xtp111 <129745214+xtp111@users.noreply.github.com>
2026-03-10 11:02:41 +0800

    first submit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <footer>
-      <p>&copy; 2026 All rights reserved by Tianpeng Xu <Link to="/credits">Credits</Link></p>
+      <p>&copy; 2026 All rights reserved by Tianpeng Xu <Link to="/">Credits</Link></p>
     </footer>
   );
 }
